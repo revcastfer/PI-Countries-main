9c0057916fcd40a71e2137da1b69b31e33bae399
fer <revcast@hotmail.com>
2023-04-30 02:16:27 -0500

    vamos guardar la actividad

--- client/src/components/formActivities.jsx
+++ client/src/components/formActivities.jsx
@@ -1,11 +1,9 @@
 import Combobox from "react-widgets/Combobox";
-import Multiselect from "react-widgets/Combobox";
-import "react-widgets/styles.css";
 import { useDispatch,useSelector } from 'react-redux'
 import {useState,useEffect} from 'react'
 import Select from 'react-select'
 import{ useNavigate,NavLink } from "react-router-dom";
-
+import axios from 'axios';
 
 export default function FormActivities(){
 
@@ -17,64 +15,104 @@ let islogin=useSelector(state=>state.islogin);
 let countries=useSelector(state=>state.countries);
 let [namesCountries,setNamecountries]=useState([]);
 let [countriesSelected,setCountriesSelected]=useState([]);
+let [errors,setErrors]=useState({name:"" ,dificultad:"" ,duracion:"" ,temporada:"" ,paises:"",validate:0});
+
 
 let names=[];
-countries.map(country=>names.push(country.name));
+countries.map(country=>names .push(country.name));
 
 
 let handleSubmit=(e)=>{
 e.preventDefault();
+if(errors.validate==1){
+axios.post("http://localhost:3001/activities",{ }    ) }
 
+};
+
+let deleteCountry=(e)=>{
+let name=e.target.id;
+
+let newCountries=countriesSelected.filter(ele=>ele!=name);
+setCountriesSelected(newCountries)
 
 }
 
 
+let validate=(e)=>{
+let validateName=document.getElementById("nombre").value;
+let validateDificultad=document.getElementById("dificultad").value;
+let validateDuracion=document.getElementById("duracion").value;
+let validateTemporada=document.getElementById("temporada").value;
+
+
+if(!validateName){ setErrors({...errors,name:"ingrese nombre"}) } else  { setErrors({...errors,name:""}) };
+if(validateDificultad==""){ setErrors({...errors,dificultad:"ingrese dificultad"}) } else { setErrors({...errors,dificultad:""}) };
+if(validateDuracion==""){ setErrors({...errors,duracion:"ingrese durcion"}) } else { setErrors({...errors,duracion:""}) };
+if(validateTemporada==""){ setErrors({...errors,temporada:"ingrese temporada"}) } else { setErrors({...errors,temporada:""}) };
+if(countriesSelected.length==0){ setErrors({...errors,paises:"ingrese paises"}) } else { setErrors({...errors,paises:""}) };
+
+if(errors.name=="" && errors.dificultad=="" && errors.duracion=="" && errors.temporada=="" && errors.paises=="")
+    {setErrors({...errors,validate:1})} else {setErrors({...errors,validate:0})}
+console.log(countriesSelected);console.log(errors);
+}
+
+
 let countryAdd=(e)=>{
-setCountriesSelected([...countriesSelected,e.target.value]);
-console.log(countriesSelected)
+let ele=e.target.value;
+if(!countriesSelected.includes(ele)) {setCountriesSelected([...countriesSelected,ele]);}
+validate(e)
 };
+
+
 
 useEffect(()=>{if(!islogin) {navigate("/")}},[countriesSelected])
 
 	return(
 <div>    
 <form onSubmit={handleSubmit}>
-<div><label>nombre :</label> <input type="text"/></div>
-<div><label>dificultad :</label> 
-<select name="dificultad" id="dificultad">
-  <option value="" readonly hidden>escoja la dificultad</option>
-  <option value="1">1</option>
-  <option value="2">2</option>
-  <option value="3">3</option>
-  <option value="4">4</option>
-</select>  
+
+<div>
+<label>nombre :</label> <input id="nombre" onChange={validate} type="text" /><label>{errors.name}</label></div>
+
+<div><label>dificultad :</label><select onChange={validate} name="dificultad" id="dificultad">
+                                    <option value="" readOnly hidden>escoja la dificultad</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                </select> <label>{errors.dificultad}</label>
 </div>
 
-<div><label>duracion :</label><input type="time" /></div>
 <div>
-<label>temporada : </label>  
-<select name="temporada" id="temporada">
- <option value="" readonly hidden>escoja la la estacion</option>
-  <option value="Verano">Verano</option>
-  <option value="Otoño" >Otoño</option>
-  <option value="Invierno">Invierno</option>
-  <option value="Primavera">Primavera</option>
-</select>
+<label>duracion :</label ><input id="duracion" onChange={validate} type="time" /><label>{errors.duracion}</label></div>
+<div><label>temporada : </label> <select onChange={validate} name="temporada" id="temporada">
+                                    <option value="" readOnly hidden>escoja la la estacion</option>
+                                    <option value="Verano">Verano</option>
+                                    <option value="Otoño" >Otoño</option>
+                                    <option value="Invierno">Invierno</option>
+                                    <option value="Primavera">Primavera</option>
+                                </select> <label>{errors.temporada}</label>
 </div>
 
 <div>
 <select  name="pais" id="pais" onChange={countryAdd}>
- <option value="" readonly hidden>escoja sus paises</option>
-{countries.map(ele=><option name={ele.name} value={ele.name}>{ele.name}</option> )}
-</select>
-
+              <option value="" readOnly hidden>escoja sus paises</option>
+             {countries.map(ele=><option name={ele.name} value={ele.name}>{ele.name}</option> )}
+     </select><label>{errors.paises}</label>
 </div>
+
 <button>create</button>
 </form>
 
 <NavLink  to="/Home" ><button>back to Home</button></NavLink>
 
 {countriesSelected.map(ele=><div></div>)}
+
+
+<div id="containerCountries">
+{countriesSelected.map(ele=><div> <div>{ele}</div><span onClick={deleteCountry} id={ele} >x</span> </div> )}
+
+</div>
 
 </div>)
 }
